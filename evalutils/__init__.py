ddb53cb71b80b964b9ec5cddd8868898a23abb7d
James Meakin <jamesmeakin@gmail.com>
2018-03-23 13:23:16 +0100

    Corrects __version__ import

--- evalutils/__init__.py
+++ evalutils/__init__.py
@@ -3,5 +3,5 @@
 __author__ = """James Meakin"""
 __email__ = 'jamesmeakin@gmail.com'
 
-from . import __version__  # noqa: F401
+from .__version__ import __version__  # noqa: F401
 from .evalutils import Evaluation  # noqa: F401
