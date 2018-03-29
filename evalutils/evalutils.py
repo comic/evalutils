7ec6139522780d41be0b54a0ad4ff5dfd6c7786b
James Meakin <jamesmeakin@gmail.com>
2018-03-29 14:13:54 +0200

    Implements data frame merging and cross validation

--- evalutils/evalutils.py
+++ evalutils/evalutils.py
@@ -4,9 +4,9 @@ from abc import ABC
 from pathlib import Path
 from typing import Tuple
 
-from pandas import DataFrame
+from pandas import DataFrame, merge
 
-from evalutils.exceptions import FileLoaderError
+from evalutils.exceptions import FileLoaderError, ValidationError
 from evalutils.io import first_int_in_filename_key, FileLoader
 from evalutils.validators import DataFrameValidator
 
@@ -19,19 +19,23 @@ class Evaluation(ABC):
         predictions_path: Path = Path('/input/'),
         file_sorter_key=first_int_in_filename_key,
         file_loader: FileLoader,
-        output_file: Path = Path('/output/metrics.json'),
         validators: Tuple[DataFrameValidator, ...] = (),
+        join_column: str = None,
+        output_file: Path = Path('/output/metrics.json'),
     ):
         super().__init__()
         self._ground_truth_path = ground_truth_path
         self._predictions_path = predictions_path
         self._file_sorter_key = file_sorter_key
         self._file_loader = file_loader
-        self._output_file = output_file
+        self._join_column = join_column
         self._validators = validators
+        self._output_file = output_file
 
         self._ground_truth_cases = DataFrame()
         self._predictions_cases = DataFrame()
+
+        self._cases = DataFrame()
 
         self._case_results = DataFrame()
         self._aggregate_results = {}
@@ -46,7 +50,8 @@ class Evaluation(ABC):
     def evaluate(self):
         self.load()
         self.validate()
-        self.join_ground_truth_and_predictions()
+        self.merge_ground_truth_and_predictions()
+        self.cross_validate()
         self.score()
         self.save()
 
@@ -67,23 +72,75 @@ class Evaluation(ABC):
                     ignore_index=True,
                 )
             except FileLoaderError:
+                # Couldn't load this file with this loader, but don't worry
+                # about it. This will be found in the validation.
                 pass
         return cases
 
     def validate(self):
         self._validate_data_frame(df=self._ground_truth_cases)
         self._validate_data_frame(df=self._predictions_cases)
-        self.cross_validate()
 
     def _validate_data_frame(self, *, df: DataFrame):
         for validator in self._validators:
             validator.validate(df=df)
 
-    def cross_validate(self):
-        pass
+    def merge_ground_truth_and_predictions(self):
+        if self._join_column:
+            kwargs = {'on': self._join_column}
+        else:
+            kwargs = {'left_index': True, 'right_index': True}
 
-    def join_ground_truth_and_predictions(self):
-        pass
+        self._cases = merge(
+            left=self._ground_truth_cases,
+            right=self._predictions_cases,
+            indicator=True,
+            how='outer',
+            suffixes=('ground_truth', 'prediction'),
+            **kwargs,
+        )
+
+    def cross_validate(self):
+        missing = [p for _, p in self._cases.iterrows() if
+                   p['_merge'] == 'left_only']
+        extra = [p for _, p in self._cases.iterrows() if
+                 p['_merge'] == 'right_only']
+
+        if missing:
+            self._raise_missing_predictions_error(missing=missing)
+
+        if extra:
+            self._raise_extra_predictions_error(extra=extra)
+
+    def _raise_missing_predictions_error(self, *, missing):
+        if self._join_column:
+            missing = [p[self._join_column] for p in missing]
+            message = (
+                'Predictions missing: you did not submit predictions for '
+                f'{self._join_column}: {missing}. Please try again.'
+            )
+        else:
+            message = (
+                'Predictions missing: you did not submit enough predictions,'
+                'please try again.'
+            )
+
+        raise ValidationError(message)
+
+    def _raise_extra_predictions_error(self, *, extra):
+        if self._join_column:
+            extra = [p[self._join_column] for p in extra]
+            message = (
+                'Too many predictions: we do not have the ground truth data '
+                f'for {self._join_column}: {extra}. Please try again.'
+            )
+        else:
+            message = (
+                'Too many predictions: you submitted too many predictions, '
+                'please try again.'
+            )
+
+        raise ValidationError(message)
 
     def score(self):
         pass
