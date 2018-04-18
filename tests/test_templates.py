d3774a90648296d82b8db3ec2afd80d8480428ce
James Meakin <jamesmeakin@gmail.com>
2018-04-18 15:41:16 +0200

    Fix test

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import fileinput
 import os
 import subprocess
 
@@ -18,6 +19,15 @@ def test_cli(tmpdir):
     assert f"Created project {project_name}" in out.decode()
 
     project_dir = os.path.join(tmpdir, project_name)
+
+    # Chicken and egg, the generated requirements.txt with fixed versions does
+    # not work if the latest version of the package is not yet on pypi, and
+    # we cannot get the latest version there unless all the tests pass. So,
+    # remove the version pinning
+    for line in fileinput.input(
+        f"{project_dir}/requirements.txt", inplace=True
+    ):
+        print(line.split('==')[0])
 
     out = subprocess.check_output(["./build.sh"], cwd=project_dir)
 
