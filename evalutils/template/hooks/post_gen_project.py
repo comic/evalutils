3564d0596e291a44a256bc2427076601bb4a898f
James Meakin <jamesmeakin@gmail.com>
2018-09-12 10:30:16 +0200

    Convert windows and mac line endings
    Fixes #27

--- evalutils/template/hooks/post_gen_project.py
+++ evalutils/template/hooks/post_gen_project.py
@@ -3,11 +3,15 @@ import os
 import shutil
 from pathlib import Path
 
-templated_python_files = Path(os.getcwd()).glob('*.py.j2')
+CHALLENGE_KIND = "{{ cookiecutter.challenge_kind }}"
+
+EOL_UNIX = b"\n"
+EOL_WIN = b"\r\n"
+EOL_MAC = b"\r"
+
+templated_python_files = Path(os.getcwd()).glob("*.py.j2")
 for f in templated_python_files:
     shutil.move(f.name, f.stem)
-
-challenge_kind = "{{ cookiecutter.challenge_kind }}"
 
 
 def remove_classification_files():
@@ -29,11 +33,38 @@ def remove_detection_files():
     os.remove(Path("test") / "detection-submission.csv")
 
 
-if challenge_kind.lower() != "segmentation":
+if CHALLENGE_KIND.lower() != "segmentation":
     remove_segmentation_files()
 
-if challenge_kind.lower() != "detection":
+if CHALLENGE_KIND.lower() != "detection":
     remove_detection_files()
 
-if challenge_kind.lower() != "classification":
+if CHALLENGE_KIND.lower() != "classification":
     remove_classification_files()
+
+
+def convert_line_endings():
+    """ Enforce unix line endings for the generated files """
+    files = []
+    for ext in [
+        ".py",
+        ".sh",
+        "Dockerfile",
+        ".txt",
+        ".csv",
+        ".mhd",
+        ".gitignore",
+    ]:
+        files.extend(Path(".").glob(f"**/*{ext}"))
+
+    for file in files:
+        with open(file, "rb") as f:
+            lines = f.read()
+
+        lines = lines.replace(EOL_WIN, EOL_UNIX).replace(EOL_MAC, EOL_UNIX)
+
+        with open(file, "wb") as f:
+            f.write(lines)
+
+
+convert_line_endings()
