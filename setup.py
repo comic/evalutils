0e9aec02ca36a6761fcdeadaf06fb124d2e69481
James Meakin <code@jmsmkn.com>
2020-02-11 21:08:36 +0100

    Adds python 3.8

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="evalutils helps challenge administrators easily create evaluation containers for grand-challenge.org.",
     install_requires=requirements,
