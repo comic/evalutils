e66cc1663c12b3a919e4348b71c745d56d01ceef
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2022-02-15 12:58:19 +0100

    Add tests on Python 3.10

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description=(
         "evalutils helps users create extensions for grand-challenge.org."
