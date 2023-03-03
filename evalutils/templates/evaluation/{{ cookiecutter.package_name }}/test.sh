c51a2bd12137fd9fbad1f8a7d1fce8bdd9e57eb8
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2023-03-03 15:09:35 +0100

    Unify test.sh

--- evalutils/templates/evaluation/{{ cookiecutter.package_name }}/test.sh
+++ evalutils/templates/evaluation/{{ cookiecutter.package_name }}/test.sh
@@ -5,13 +5,17 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 ./build.sh
 
 VOLUME_SUFFIX=$(dd if=/dev/urandom bs=32 count=1 | md5sum | cut --delimiter=' ' --fields=1)
+{% if cookiecutter.template_kind == "Algorithm" -%}
+# Maximum is currently 30g, configurable in your algorithm image settings on grand challenge
+{%- endif %}
+MEM_LIMIT="4g"
 
 docker volume create {{ cookiecutter.package_name|lower }}-output-$VOLUME_SUFFIX
 
 # Do not change any of the parameters to docker run, these are fixed
 docker run --rm \
-        --memory="4g" \
-        --memory-swap="4g" \
+        --memory="${MEM_LIMIT}" \
+        --memory-swap="${MEM_LIMIT}" \
         --network="none" \
         --cap-drop="ALL" \
         --security-opt="no-new-privileges" \
@@ -24,5 +28,18 @@ docker run --rm \
 docker run --rm \
         -v {{ cookiecutter.package_name|lower }}-output-$VOLUME_SUFFIX:/output/ \
         python:{{ cookiecutter.python_major_version }}.{{ cookiecutter.python_minor_version }}-slim cat /output/metrics.json | python -m json.tool
+
+{% if cookiecutter.template_kind == "Algorithm" -%}
+docker run --rm \
+        -v {{ cookiecutter.package_name|lower }}-output-$VOLUME_SUFFIX:/output/ \
+        -v $SCRIPTPATH/test/:/input/ \
+        python:{{ cookiecutter.python_major_version }}.{{ cookiecutter.python_minor_version }}-slim python -c "import json, sys; f1 = json.load(open('/output/results.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
+
+if [ $? -eq 0 ]; then
+    echo "Tests successfully passed..."
+else
+    echo "Expected output was not found..."
+fi
+{%- endif %}
 
 docker volume rm {{ cookiecutter.package_name|lower }}-output-$VOLUME_SUFFIX
